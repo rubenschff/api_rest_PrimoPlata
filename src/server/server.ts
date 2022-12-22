132a0f93c9094d4658bd9545d7c726bc0412fcfc
Rubens Christoff <rubenschristoff@gmail.com>
2022-12-22 11:32:22 -0300

    configurando rotas

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,7 +1,9 @@
 import express from 'express'
+import { router } from './routes'
 
-const server = express(); server.get('/', (req, res) => {
-    return res.send('Ola, Dev');
-});
+const server = express(); 
+
+server.use(router)
+server.use(express.json())
 
 export { server };
