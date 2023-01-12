45873db7c6b3b703eed1d3dcddc695ce6e36e25b
Rubens Christoff <rubenschristoff@gmail.com>
2023-01-11 23:23:15 -0300

    Controller de usuário

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
+import { UsuariosController } from "../controllers";
+import {StatusCodes} from "http-status-codes"
+
 
 const router = Router();
 
 router.get('/', (req, res) => {
   return res.send('Ola, Dev');
 });
+
+
+router.post('/usuario', UsuariosController.create);
 
 
 
