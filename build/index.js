40cd4c3643614bc8aad908566495f80b9b632cf4
Rubens Christoff <rubenschristoff@gmail.com>
2023-01-14 01:49:58 -0300

    Configurando testes

--- build/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./server/server");
-server_1.server.listen(3333, () => console.log('App rodando!'));
