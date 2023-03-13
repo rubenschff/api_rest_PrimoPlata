ae1a7cbb726f1ea7e3cd37ed3a08839fec77af5c
Rubens Christoff <rubenschristoff@gmail.com>
2023-03-13 15:07:17 -0300

    gerando accessToken apenas se autenticar usuário

--- src/server/database/providers/usuario/create.ts
+++ src/server/database/providers/usuario/create.ts
@@ -3,15 +3,17 @@ import  { Knex} from "../../knex";
 import {ETableNames} from "../../ETableNames";
 import { passwordCrypto } from "../../../shared/services";
 import {JWTservice} from "../../../shared/services/JWTservice";
+import * as http from "http";
 
 
 export const create = async (usuario: Omit<IUsuario, 'id'>): Promise<object| Error> => {
     try {
         const hashedPassword = await passwordCrypto.hashPassword(usuario.password!);
         const [result] = await Knex(ETableNames.usuario).insert({...usuario, password: hashedPassword}).returning('id');
-        const accessToken = JWTservice.sign({uid: result.id})
+
 
         if(typeof result === 'object') {
+            const accessToken = JWTservice.sign({uid: result.id})
             return {id:result.id,accessToken: accessToken};
         }
 
