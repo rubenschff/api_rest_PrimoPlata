7a1ff1e73386f3d1b45ee5223f6e71778c871535
Rubens Christoff <rubenschristoff@gmail.com>
2023-03-29 22:11:14 -0300

    Enum das rotas

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -9,6 +9,7 @@ import {
 import { StatusCodes } from "http-status-codes";
 import { autenticateRoutes } from "../shared/middleware";
 import {RespostasController} from "../controllers/respostas";
+import {RoutesEnum} from "./routes.enum";
 
 const router = Router();
 
@@ -16,40 +17,46 @@ router.get("/", (req, res) => {
   return res.send("Ola, Dev");
 });
 
+
+//-------------------------------------------------Login e Cadastro-------------------------------------------------
+router.post(RoutesEnum.cadastrar,UsuariosController.createValidation,UsuariosController.create);
+router.post(RoutesEnum.entrar, UsuariosController.loginValidation,UsuariosController.login);
+
+
+//-------------------------------------------------Rota de usuário-------------------------------------------------
 // @ts-ignore
-router.get("/usuario",autenticateRoutes,UsuariosController.getByIdValidation,UsuariosController.getById);
+router.get(RoutesEnum.usuario,autenticateRoutes,UsuariosController.getByIdValidation,UsuariosController.getById);
 // @ts-ignore
-router.put("/usuario/:id",autenticateRoutes,UsuariosController.updateByIdValidation,UsuariosController.updateById);
+router.put(RoutesEnum.usuario+"/:id",autenticateRoutes,UsuariosController.updateByIdValidation,UsuariosController.updateById); //todo ajustar pra fazer alteração com SESSION_ID do cookie
 // @ts-ignore
-router.delete("/usuario/:id",autenticateRoutes,UsuariosController.deleteByIdValidation,UsuariosController.deleteById);
-
-router.post("/cadastrar",UsuariosController.createValidation,UsuariosController.create);
-
-router.post("/entrar", UsuariosController.loginValidation,UsuariosController.login);
-
-//investimentos
-router.post("/investimento", InvestimentoController.createValidation, InvestimentoController.create);
-router.get("/investimento", InvestimentoController.getAllValidation, InvestimentoController.getAll);
+router.delete(RoutesEnum.usuario+"/:id",autenticateRoutes,UsuariosController.deleteByIdValidation,UsuariosController.deleteById); //todo ajustar pra fazer alteração com SESSION_ID do cookie
 
 
-//perguntas
-router.post("/perguntas", PerguntasController.createValidation, PerguntasController.create);
-router.get("/perguntas", PerguntasController.getAllValidation, PerguntasController.getAll);
+//-------------------------------------------------Investimentos-------------------------------------------------
+router.post(RoutesEnum.investimento, InvestimentoController.createValidation, InvestimentoController.create);
+router.get(RoutesEnum.investimento, InvestimentoController.getAllValidation, InvestimentoController.getAll);
+
+
+//-------------------------------------------------Perguntas-------------------------------------------------
+router.post(RoutesEnum.perguntas, PerguntasController.createValidation, PerguntasController.create);
+router.get(RoutesEnum.perguntas,autenticateRoutes, PerguntasController.getAllValidation, PerguntasController.getAll);
 // @ts-ignore
-router.delete("/perguntas/:id", PerguntasController.deleteByIdValidation, PerguntasController.deleteById);
+router.delete(RoutesEnum.perguntas+"/:id", PerguntasController.deleteByIdValidation, PerguntasController.deleteById); //todo ajustar pra fazer alteração com SESSION_ID do cookie
 
-//alternativa
-router.post("/alternativa", AlternativaController.createValidation, AlternativaController.create);
 
-//comparativo
+//-------------------------------------------------Alternativas-------------------------------------------------
+router.post(RoutesEnum.alternativa, AlternativaController.createValidation, AlternativaController.create);
+
+
+//-------------------------------------------------Comparativo-------------------------------------------------
 // @ts-ignore
-router.put("/comparativo/:id", ComparativoController.updateByIdValidation, ComparativoController.updateByUserId);
-router.post("/comparativo", ComparativoController.createValidation, ComparativoController.create);
+router.put(RoutesEnum.comparativo+"/:id", ComparativoController.updateByIdValidation, ComparativoController.updateByUserId); //todo ajustar pra fazer alteração com SESSION_ID do cookie
+router.post(RoutesEnum.comparativo, ComparativoController.createValidation, ComparativoController.create);
 // @ts-ignore
-router.get("/comparativo/:userId", ComparativoController.getByUserIdValidation, ComparativoController.getByUserId);
+router.get(RoutesEnum.comparativo+"/:userId", ComparativoController.getByUserIdValidation, ComparativoController.getByUserId); //todo ajustar pra fazer alteração com SESSION_ID do cookie
 
-//-------------------------------------------------Perguntas Respostas-------------------------------------------------
-router.post("/perguntas/resposta", RespostasController.createValidation, RespostasController.create);
+//-------------------------------------------------Respostas-------------------------------------------------
+router.post(RoutesEnum.resposta, RespostasController.createValidation, RespostasController.create);
 
 export { router };
  
---
+++ src/server/routes/routes.enum.ts
@@ -0,0 +1,11 @@
+
+export enum RoutesEnum {
+    usuario = '/usuario',
+    investimento = '/investimento',
+    perguntas = '/perguntas',
+    alternativa = '/alternativa',
+    comparativo = '/comparativo',
+    resposta = '/resposta',
+    cadastrar = '/cadastrar',
+    entrar = '/entrar'
+}
