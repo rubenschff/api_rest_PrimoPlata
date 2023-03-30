540cb06815b428dbc9e264dffe295360bdad0fa6
Rubens Christoff <rubenschristoff@gmail.com>
2023-03-30 14:20:20 -0300

    Ajuste no provider de getById de usuário

--- src/server/database/providers/usuario/getById.ts
+++ src/server/database/providers/usuario/getById.ts
@@ -1,13 +1,17 @@
-import { ETableNames } from "../../ETableNames";
+import {ETableNames, UsuarioTable} from "../../ETableNames";
 import { Knex } from "../../knex";
 import { IUsuario } from "../../models";
 
 export const getById = async (id: number): Promise<IUsuario | Error> => {
   try {
-    const result = await Knex(ETableNames.usuario)
-      .select("id","name","nickName","dateOfBirth")
+    const result:IUsuario = await Knex(ETableNames.usuario)
+      .select(UsuarioTable.id,
+          UsuarioTable.name,
+          UsuarioTable.nickName,
+          UsuarioTable.password,
+          UsuarioTable.dateOfBirth)
       .from(ETableNames.usuario)
-      .where("id", "=", id)
+      .where(UsuarioTable.id, id)
       .first();
 
     if (result) return result;
