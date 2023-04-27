1baf5b071a84e378c13351c3c6e32f1eb5728a0f
Rubens Christoff <rubenschristoff@gmail.com>
2023-04-27 16:30:12 -0300

    Informar senha para alterar dados

--- src/server/controllers/usuarios/updateById.ts
+++ src/server/controllers/usuarios/updateById.ts
@@ -5,10 +5,13 @@ import { validation } from "../../shared/middleware";
 import {CookieDto, IUsuario} from "../../database/models";
 import { UsuarioProvider } from "../../database/providers/usuario";
 import {JWTservice} from "../../shared/services/JWTservice";
+import {passwordCrypto} from "../../shared/services";
 
 interface IHeaderProperties extends CookieDto{ }
 
-interface IBodyPropeties extends Omit<IUsuario, 'id'|'dateOfBirth' >{ }
+interface IBodyPropeties extends Omit<IUsuario, 'id'|'dateOfBirth' >{
+    oldPassword?: string;
+}
 
 export const updateByIdValidation = validation((getSchema) => ({
   header: getSchema<IHeaderProperties>(yup.object().shape({
@@ -17,7 +20,8 @@ export const updateByIdValidation = validation((getSchema) => ({
   body: getSchema<IBodyPropeties>(yup.object().shape({
     name: yup.string().notRequired().min(3).max(150),
     nickName: yup.string().notRequired().min(6),
-    password: yup.string().notRequired().min(4)
+    password: yup.string().notRequired().min(4),
+      oldPassword: yup.string().required().min(4)
   })),
 }));
 
@@ -35,7 +39,29 @@ export const updateById = async (req: Request<IHeaderProperties,{},IBodyPropetie
     const auth = JWTservice.verify(req.headers.authorization!)
 
     if (typeof auth === 'object'){
-        const update = await UsuarioProvider.updateById(auth.uid,req.body);
+
+        const oldUser = await UsuarioProvider.getById(auth.uid)
+
+        if (oldUser instanceof Error){
+           return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+                default:{
+                    error: oldUser.message
+                }
+            });
+        }
+
+        const oldPassword = await passwordCrypto.verifyPassword(req.body.oldPassword!, oldUser.password!)
+
+        if (!oldPassword){
+            return res.status(StatusCodes.BAD_REQUEST).json({
+                error: 'Senha incorreta'
+            });
+        }
+
+
+        delete req.body.oldPassword
+
+        const update = await UsuarioProvider.updateById(auth.uid, req.body);
 
         if(update instanceof Error){
             console.log(update);
