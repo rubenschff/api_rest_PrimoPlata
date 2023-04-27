f138c0fc03da32aed202533d3a5a9cc53c641d3a
Rubens Christoff <rubenschristoff@gmail.com>
2023-04-27 17:30:47 -0300

    venda de ativos

--- src/server/controllers/transacoes/venda.ts
+++ src/server/controllers/transacoes/venda.ts
@@ -1,7 +1,21 @@
 import {TransacaoDTO} from "../../database/models/transacao.dto";
 import {IFinanceiroDTO} from "../../database/models";
+import {InvestimentoFixoProvider} from "../../database/providers/investimento_compra_venda";
 
 
 export const venda = (transacao: Omit<TransacaoDTO, 'id'>,financeiro: Omit<IFinanceiroDTO, 'id'>) => {
-  
+    if (transacao.investimentoId <= 3) {
+
+        if(!transacao.valorTransacao){
+            return Error('valorTransacao precisa ser informado para investimentos fixos!');
+        }
+
+
+
+    }else {
+        if (!transacao.quantidadeCotas || !transacao.valorCota) {
+            return Error('quantidadeCotas e valorCota precisam ser informados em investimentos variáveis!');
+        }
+        return Error('compra de transação variavel não implementado')//todo Service de investimento variavel
+    }
 }
